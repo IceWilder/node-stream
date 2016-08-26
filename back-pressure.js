65ea833312034208ff8ec15c726cf1a808995c58
Icewilder <271497566@qq.com>
2016-08-26 18:00:04 +0800

    backpressure

--- back-pressure.js
+++ back-pressure.js
@@ -2,10 +2,10 @@ const stream = require('stream')
 
 var c = 0
 const readable = stream.Readable({
-  highWaterMark: 2,
+  highWaterMark: 6,
   read: function () {
     process.nextTick(() => {
-      var data = c < 6 ? String.fromCharCode(c + 65) : null
+      var data = c < 20 ? String.fromCharCode(c + 65) : null
       console.log('push', ++c, data)
       this.push(data)
     })
@@ -13,8 +13,9 @@ const readable = stream.Readable({
 })
 
 const writable = stream.Writable({
-  highWaterMark: 2,
+  highWaterMark: 6,
   write: function (chunk, enc, next) {
+    next();
     console.log('write', chunk)
   }
 })
